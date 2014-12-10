7212f738b146334b7b9911a89766e6963b8a5621
Daniel <djalova1@jhu.edu>
2014-12-10 18:44:04 -0500

    Added python script to query AngelList along with some of the results in JSON

---
+++ PythonScripts/read.py
@@ -0,0 +1,25 @@
+import os,sys
+import urllib
+from angellist import AngelList
+
+def main():
+#	angel = AngelList("6093fd1f428b52cd5e5f039e63b4870ce3b3c5884ff35e32")
+
+	angel = AngelList("6093fd1f428b52cd5e5f039e63b4870ce3b3c5884ff35e49")
+#	angelapi.search({'method':'GET', 'query':'search-string'})
+
+# 	print angel.startups({'method':'GET', 'id':str(520)})
+
+	for x in range(1001, 1200):
+		try:
+		 	print angel.startups({'method':'GET', 'id':str(x)})
+		except Exception:
+			pass
+
+
+
+	
+
+
+if __name__ == "__main__":
+	main()
