c0a68c4347b823b660d31b6a2b73ed1267553dff
Daniel <djalova1@jhu.edu>
2014-12-12 18:29:10 -0500

    Added startups 1001 to 2000

--- PythonScripts/read.py
+++ PythonScripts/read.py
@@ -3,21 +3,17 @@ import urllib
 from angellist import AngelList
 
 def main():
-#	angel = AngelList("6093fd1f428b52cd5e5f039e63b4870ce3b3c5884ff35e32")
+	angel = AngelList("6093fd1f428b52cd5e5f039e63b4870ce3b3c5884ff35e32")
 
-	angel = AngelList("6093fd1f428b52cd5e5f039e63b4870ce3b3c5884ff35e49")
+#	angel = AngelList("6093fd1f428b52cd5e5f039e63b4870ce3b3c5884ff35e49")
 #	angelapi.search({'method':'GET', 'query':'search-string'})
 
-# 	print angel.startups({'method':'GET', 'id':str(520)})
-
-	for x in range(1001, 1200):
+	# Enter range of IDs here
+	for x in range(1001, 2000):
 		try:
 		 	print angel.startups({'method':'GET', 'id':str(x)})
 		except Exception:
 			pass
-
-
-
 	
 
 
