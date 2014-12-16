323ce5d776ce149c2cc0aee3c44213bf63d0da96
Daniel Jalova <djalova1@jhu.edu>
2014-12-15 23:43:14 -0500

    Initial commit for webpage.

---
+++ Webpage/startupList.php
@@ -0,0 +1,21 @@
+<html> 
+<link rel="stylesheet" href="style.css">
+<body>
+	<div class = "header">
+		
+    <h1 style = "padding-top:30px">Final Database Project</h1>
+    <h2 style = "padding-bottom:-10px">By Daniel Jalova and Samantha Siow</h2>
+
+    	<div class = "searchBox">
+			<form action="action_page.php">
+			<input type="text" name="result" value="">
+			<input type="submit" value="Search">
+			</form>
+
+		</div>
+	</div>
+
+	
+    
+</html> 
+</body> 
