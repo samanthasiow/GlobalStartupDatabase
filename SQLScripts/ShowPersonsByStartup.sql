0a9438097b565430cbd4ca30d18c1775ab2cbfbb
Samantha Siow <ssiow1@jhu.edu>
2014-12-18 15:36:13 -0500

    fixed an error message and made it search by id

--- SQLScripts/ShowPersonsByStartup.sql
+++ SQLScripts/ShowPersonsByStartup.sql
@@ -2,15 +2,15 @@ DELIMITER $$
 
 DROP PROCEDURE IF EXISTS ShowPersonsByStartup $$
 
-CREATE PROCEDURE ShowPersonsByStartup(IN startupName VARCHAR(255))
+CREATE PROCEDURE ShowPersonsByStartup(IN startupID VARCHAR(255))
 BEGIN
-	IF EXISTS (SELECT * FROM StartupListing as SL WHERE SL.startupName = startupName)
+	IF EXISTS (SELECT * FROM StartupListing as SL WHERE SL.id = startupID)
 		THEN 
 			SELECT name as Name, role as Role, startupName as StartupName
 			FROM StartupListing as S, StartupRoles as R
-			WHERE R.startupID = S.id and S.startupName = startupName;
+			WHERE R.startupID = S.id and S.id = startupID;
 	ELSE
-		SELECT 'No Startups found in that Market.' AS  'Error Message';
+		SELECT 'No people found for that startup.' AS  'Error Message';
 	END IF;
 END
 $$
