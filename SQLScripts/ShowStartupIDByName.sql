d605b28e375e323798178b7e3079ae3b3ed2619a
Samantha Siow <ssiow1@jhu.edu>
2014-12-18 16:46:40 -0500

    hahAHhAhAHA ha hA hAh hA ha ha

--- SQLScripts/ShowStartupIDByName.sql
+++ SQLScripts/ShowStartupIDByName.sql
@@ -2,15 +2,15 @@ DELIMITER $$
 
 DROP PROCEDURE IF EXISTS ShowStartupByID $$
 
-CREATE PROCEDURE ShowStartupByID(IN startupID INT)
+CREATE PROCEDURE ShowStartupByID(IN startupName VARCHAR(255))
 BEGIN
 	IF EXISTS (SELECT * FROM StartupListing as SL WHERE SL.id = startupID)
 		THEN 
 			SELECT startupName as Name
 			FROM StartupListing as S
-			WHERE S.id = startupID;
+			WHERE S.name = startupName;
 	ELSE
-		SELECT 'No people found for that startup.' AS  'Error Message';
+		SELECT 'No startups found for that ID.' AS  'Error Message';
 	END IF;
 END
 $$
