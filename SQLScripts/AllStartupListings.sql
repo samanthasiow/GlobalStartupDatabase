dc2efd7a56c5acca2913e339092bce893831a2b5
Daniel Jalova <djalova1@jhu.edu>
2014-12-18 13:50:59 -0500

    Modified AllStartupListings to return ids.

--- SQLScripts/AllStartupListings.sql
+++ SQLScripts/AllStartupListings.sql
@@ -4,7 +4,7 @@ DROP PROCEDURE IF EXISTS AllStartupListings $$
 
 CREATE PROCEDURE AllStartupListings()
 BEGIN
-	SELECT startupName as StartupName, highConcept as HighConcept, companyUrl as URL
+	SELECT id, startupName as StartupName, highConcept as HighConcept, companyUrl as URL, thumbUrl as imgURL
     FROM StartupListing;
 END
 $$
